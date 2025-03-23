6bc3dfa04778c5b75a0817ba9df334dff1bd166b
Kamil Golec <kumalgfilms@gmail.com>
2025-03-23 02:24:10 +0100

    Add song names and times

--- src/playlists/restrictedGroupChat.ts
+++ src/playlists/restrictedGroupChat.ts
@@ -257,21 +257,215 @@ export const PLAYLIST = new Map<string, { [key in DeviceKey]?: Effect }>(Object.
 
 
 // Time	Song Name
-// 46:16	KX CHR - Yasuo
-// 47:27	TRFN & Ken Rosenberg - Maneater
-// 49:53	Restricted & Nik Sitz ft. 89ers - Party People
-// 53:02	T A N E - MONEY RIGHT
-// 54:23	Vortek's - Crashdown
-// 56:11	Scot Project & Sam Jones - Weed
-// 59:42	MANDY - UNDRGRND (2024 Rave Edit)
-// 1:01:02	TNT - 2003
-// 1:02:14	GNS & David DTX - My Humps
-// 1:03:14	Vortex's & Tekas & Spice Up! - Soldiers
-// 1:05:05	The Bloody Beetroots ft. Steve Aoki - Warp 1.9 (Edmmaro Hard Techno Mix)
-// 1:06:26	TRIPTYKH - Bass Keeps Going
-// 1:07:41	O.B.I. - Hard Music Lessons
-// 1:09:08	RÜFÜS DU SOL - Innerbloom (HOEHENANGST HT Remix)
-// 1:12:35	Coolio & 1World & Holy Priest - Gangsta's Paradise
-// 1:14:53	Disclosure ft. Eliza Doolittle - You & Me (ØTJE Hard Techno Remix)
-// 1:17:40	Ray Volpe - Laserbeam (Anderex More Lazers Bootleg)
-// Played w/	Rooler & Sickmode - CLUB BANGER
+// [
+//   {
+//       "position": "01",
+//       "time": "",
+//       "title": "Restricted - ID"
+//   },
+//   {
+//       "position": "02",
+//       "time": "01:55",
+//       "title": "Kendrick Lamar - Swimming Pools (Drank) (Lister Remix) "
+//   },
+//   {
+//       "position": "w/",
+//       "time": "",
+//       "title": "Lister - Faded (Swimming Pools Edit)"
+//   },
+//   {
+//       "position": "03",
+//       "time": "03:11",
+//       "title": "Timmy Trumpet & Restricted - Perfect (Exceeder) "
+//   },
+//   {
+//       "position": "04",
+//       "time": "05:40",
+//       "title": "Joey Risdon - Hardcore Power "
+//   },
+//   {
+//       "position": "05",
+//       "time": "06:55",
+//       "title": "KIRK - Hardstyle Flavour "
+//   },
+//   {
+//       "position": "06",
+//       "time": "08:16",
+//       "title": "Warp Brothers & Flymeon - Phatt Bass "
+//   },
+//   {
+//       "position": "07",
+//       "time": "10:40",
+//       "title": "Artemas - i like the way you kiss me "
+//   },
+//   {
+//       "position": "08",
+//       "time": "13:27",
+//       "title": "The Prodigy - Voodoo People (Mr.Machine Edit)"
+//   },
+//   {
+//       "position": "09",
+//       "time": "15:16",
+//       "title": "Restricted & KIRK - We Don't Care"
+//   },
+//   {
+//       "position": "10",
+//       "time": "18:06",
+//       "title": "Restricted & Nik Sitz - Tunnel Vision "
+//   },
+//   {
+//       "position": "11",
+//       "time": "21:51",
+//       "title": "Franky-B & Kø:lab - Godspeed "
+//   },
+//   {
+//       "position": "12",
+//       "time": "23:09",
+//       "title": "Azyr - Direct It To The Roof (Riot Code Remix) "
+//   },
+//   {
+//       "position": "13",
+//       "time": "25:34",
+//       "title": "Restricted - Close To My"
+//   },
+//   {
+//       "position": "14",
+//       "time": "28:14",
+//       "title": "Zombie Nation - Kernkraft 400 (Mr.Machine & Edmmaro Edit)"
+//   },
+//   {
+//       "position": "w/",
+//       "time": "29:56",
+//       "title": "Zombie Nation - Kernkraft 400 (MJU 2k24 Edit) "
+//   },
+//   {
+//       "position": "15",
+//       "time": "31:38",
+//       "title": "Skrillex & Habstrakt - Chicken Soup (Vortek's Remix) "
+//   },
+//   {
+//       "position": "16",
+//       "time": "34:50",
+//       "title": "GRAVEDGR - FVCK YOU!"
+//   },
+//   {
+//       "position": "17",
+//       "time": "36:47",
+//       "title": "Black Eyed Peas - Pump It "
+//   },
+//   {
+//       "position": "18",
+//       "time": "37:48",
+//       "title": "Gyko - Hypnotic Waves "
+//   },
+//   {
+//       "position": "19",
+//       "time": "39:42",
+//       "title": "Restricted - Smells Like Teen Spirit "
+//   },
+//   {
+//       "position": "20",
+//       "time": "42:42",
+//       "title": "KNTRLVRLST - Psycho "
+//   },
+//   {
+//       "position": "21",
+//       "time": "44:22",
+//       "title": "Jayron & Gewoonraves - Knock Knock "
+//   },
+//   {
+//       "position": "22",
+//       "time": "46:16",
+//       "title": "KX CHR - Yasuo "
+//   },
+//   {
+//       "position": "23",
+//       "time": "47:27",
+//       "title": "TRFN & Ken Rosenberg - Maneater "
+//   },
+//   {
+//       "position": "w/",
+//       "time": "",
+//       "title": "Benny Benassi pres. The Biz - Satisfaction (BEAUZ Hard Techno Remix) "
+//   },
+//   {
+//       "position": "24",
+//       "time": "49:53",
+//       "title": "Restricted & Nik Sitz ft. 89ers - Party People "
+//   },
+//   {
+//       "position": "25",
+//       "time": "53:02",
+//       "title": "T A N E - MONEY RIGHT "
+//   },
+//   {
+//       "position": "26",
+//       "time": "54:23",
+//       "title": "Vortek's - Crashdown "
+//   },
+//   {
+//       "position": "27",
+//       "time": "56:11",
+//       "title": "Scot Project & Sam Jones - Weed "
+//   },
+//   {
+//       "position": "28",
+//       "time": "59:42",
+//       "title": "MANDY - UNDRGRND (2024 Rave Edit) "
+//   },
+//   {
+//       "position": "29",
+//       "time": "1:01:02",
+//       "title": "TNT - 2003 "
+//   },
+//   {
+//       "position": "30",
+//       "time": "1:02:14",
+//       "title": "GNS & David DTX - My Humps "
+//   },
+//   {
+//       "position": "31",
+//       "time": "1:03:14",
+//       "title": "Vortex's & Tekas & Spice Up! - Soldiers "
+//   },
+//   {
+//       "position": "32",
+//       "time": "1:05:05",
+//       "title": "The Bloody Beetroots ft. Steve Aoki - Warp 1.9 (Edmmaro Hard Techno Mix) "
+//   },
+//   {
+//       "position": "33",
+//       "time": "1:06:26",
+//       "title": "TRIPTYKH - Bass Keeps Going "
+//   },
+//   {
+//       "position": "34",
+//       "time": "1:07:41",
+//       "title": "O.B.I. - Hard Music Lessons "
+//   },
+//   {
+//       "position": "35",
+//       "time": "1:09:08",
+//       "title": "RÜFÜS DU SOL - Innerbloom (HOEHENANGST HT Remix) "
+//   },
+//   {
+//       "position": "36",
+//       "time": "1:12:35",
+//       "title": "Coolio & 1World & Holy Priest - Gangsta's Paradise "
+//   },
+//   {
+//       "position": "37",
+//       "time": "1:14:53",
+//       "title": "Disclosure ft. Eliza Doolittle - You & Me (ØTJE Hard Techno Remix) "
+//   },
+//   {
+//       "position": "38",
+//       "time": "1:17:40",
+//       "title": "Ray Volpe - Laserbeam (Anderex More Lazers Bootleg) "
+//   },
+//   {
+//       "position": "w/",
+//       "time": "",
+//       "title": "Rooler & Sickmode - CLUB BANGER "
+//   }
+// ]
